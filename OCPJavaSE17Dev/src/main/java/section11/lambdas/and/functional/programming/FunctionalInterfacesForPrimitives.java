386f7a94fc70256d093f9fb7e039d0dd3f6a4896
Dhruv Jawalkar <dhruv.jawalkar@gmail.com>
2024-04-30 08:57:28 -0700

    completing section 11 lambdas and functional programming

---
+++ OCPJavaSE17Dev/src/main/java/section11/lambdas/and/functional/programming/FunctionalInterfacesForPrimitives.java
@@ -0,0 +1,26 @@
+package section11.lambdas.and.functional.programming;
+
+import java.util.Random;
+import java.util.function.*;
+
+public class FunctionalInterfacesForPrimitives {
+    public static void main(String[] args){
+        IntSupplier twice = () -> new Random().nextInt(10);
+        System.out.println(twice.getAsInt());
+
+        IntConsumer greet = (n) -> System.out.println("Hello, age:" + n);
+        greet.accept(24);
+
+        LongPredicate gt10 = (n) -> n > 10;
+        System.out.println(gt10.test(11));
+
+        IntFunction<Double> square = (n) -> Math.pow(n, 2);
+        System.out.println(square.apply(3));
+
+        ToDoubleFunction<Integer> cube = (n) -> Math.pow(n, 3);
+        System.out.println(cube.applyAsDouble(3));
+
+        DoubleToIntFunction truncate = n -> (int) Math.floor(n);
+        System.out.println(truncate.applyAsInt(2.3));
+    }
+}
---
+++ OCPJavaSE17Dev/src/main/java/section16/streams/UsingOptional.java
@@ -0,0 +1,7 @@
+package section16.streams;
+
+public class UsingOptional {
+    public static void main(String[] args){
+
+    }
+}
